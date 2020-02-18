16cc896c0c45878026a5572fb7fe34f1113799c6
Tamas Rudnai <tamas.rudnai@gmail.com>
2020-02-17 22:33:27 -0800

    Added iOS platform

---
+++ A2iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  A2iOS
+//
+//  Created by Tamas Rudnai on 2/17/20.
+//  Copyright © 2020 GameAlloy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ A2iOSTests/A2iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  A2iOSTests.swift
+//  A2iOSTests
+//
+//  Created by Tamas Rudnai on 2/17/20.
+//  Copyright © 2020 GameAlloy. All rights reserved.
+//
+
+import XCTest
+@testable import A2iOS
+
+class A2iOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

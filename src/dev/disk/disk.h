f09585176cf9b5fb7eea487982b25716ba2b5308
tudnai <tamas.rudnai@gmail.com>
2020-07-13 10:10:33 -0700

    Added GPL v3 License

--- src/dev/disk/disk.h
+++ src/dev/disk/disk.h
@@ -3,7 +3,22 @@
 //  A2Mac
 //
 //  Created by Tamas Rudnai on 2/15/20.
-//  Copyright © 2020 GameAlloy. All rights reserved.
+//  Copyright © 2019,2020 Tamas Rudnai. All rights reserved.
+//
+// This file is part of Steve ][ -- The Apple ][ Emulator.
+//
+// Steve ][ is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Steve ][ is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Steve ][.  If not, see <https://www.gnu.org/licenses/>.
 //
 
 #ifndef disk_h
--- src/dev/disk/dsk2woz.c
+++ src/dev/disk/dsk2woz.c
@@ -18,6 +18,21 @@
 //
 // Modified by Tamas Rudnai for Steve ][ emulator project (2020)
 //
+// This file is part of Steve ][ -- The Apple ][ Emulator.
+//
+// Steve ][ is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Steve ][ is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Steve ][.  If not, see <https://www.gnu.org/licenses/>.
+//
 
 // Forward declarations; see definitions for documentation.
 static uint32_t crc32(const uint8_t *buf, size_t size);

bc6c350c6e6633da597689114b24c002a01472e5
tudnai <tamas.rudnai@gmail.com>
2022-07-10 19:52:16 -0700

    Submenu Handler

---
+++ A2Mac/CheatMenu.swift
@@ -0,0 +1,24 @@
+//
+//  NSMenu_Hard_Hat_Mack.swift
+//  Steve ][
+//
+//  Created by Tamas Rudnai on 7/5/22.
+//  Copyright © 2022 GameAlloy. All rights reserved.
+//
+
+import Cocoa
+import Foundation
+
+
+class MainMenu: NSMenu {
+    override func update() {
+        super.update()
+        print("MainMenu update")
+    }
+    
+    override func submenuAction(_ sender: Any?) {
+        super.submenuAction(sender)
+        print("MainMenu submenuAction")
+    }
+    
+}
